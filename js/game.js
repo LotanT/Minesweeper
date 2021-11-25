c9274ef685d3cbfab4fe7ce46359222206878c4f
LotanT <95045934+LotanT@users.noreply.github.com>
2021-11-25 20:45:52 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -34,15 +34,13 @@ function initGame() {
     gBoard = buildBoard();
     setMinesNegsCount(gBoard);
     renderBoard(gBoard);
-    // localStorage.clear()
-    var savedScores = localStorage.getItem('highscore') || '[]';
-    console.log(savedScores)
-    if(savedScores === '[]'){
-        resetScoreTable();
-        console.log(savedScores)
-    }
-    renderScores();
-    showBoardOnConsole(gBoard);
+    localStorage.setItem('highscore', [])
+    // var savedScores = localStorage.getItem('highscore') || '[]';
+    // if(savedScores === '[]'){
+    //     resetScoreTable();
+    // }
+    // renderScores();
+    // showBoardOnConsole(gBoard);
     
 }
 
@@ -113,7 +111,7 @@ function gameOver(isWon, i, j) {
     var elResBtn = document.querySelector(`.resBtn`);
     if (isWon){
     elResBtn.innerText = '😎';
-    scoreList();
+    // scoreList();
 }
     else {
         var elCell = document.querySelector(`.cell-${i}-${j}`);
